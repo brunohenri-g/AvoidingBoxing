4a416beca7924119a2a80fde6bc5d4ad1f73e129
Bruno Henri <brunohenri.g@live.com>
2020-03-26 16:00:50 -0300

    Primeira implementação

---
+++ AvoidingBoxing/Models/PontoNoEspacoR3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvoidingBoxing.Models
+{
+    public class PontoNoEspacoR3
+    {
+        public double X;
+        public double Y;
+        public double Z;
+    }
+}
---
+++ AvoidingBoxing/Program.cs
@@ -0,0 +1,41 @@
+﻿using AvoidingBoxing.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AvoidingBoxing
+{
+    class Program
+    {
+        private const int pointsInSpaceR3 = 10_000_000;
+        static void Main(string[] args)
+        {
+            using Process process = Process.GetCurrentProcess();
+            Console.WriteLine($"{ConvertBytesToMegabytes(GC.GetTotalMemory(false))} used memory before.");
+
+            var points = new List<PontoNoEspacoR3>(pointsInSpaceR3);
+
+            for (var i = 0; i < pointsInSpaceR3; i++)
+            {
+                points.Add(new PontoNoEspacoR3
+                {
+                    X = i,
+                    Y = i,
+                    Z = i
+                });
+            }
+
+            Console.WriteLine($"{ConvertBytesToMegabytes(GC.GetTotalMemory(false))} used memory after.");
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadLine();
+        }
+
+        static string ConvertBytesToMegabytes(long bytes)
+        {
+            return ((bytes / 1024f) / 1024f).ToString("0.00") + " MB";
+        }
+
+    }
+
+
+}
